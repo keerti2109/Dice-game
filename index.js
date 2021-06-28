f592f662b96012c67d12a5115d20de8e3c7c1abd
Keerti Lata <77429149+keerti2109@users.noreply.github.com>
2021-06-28 12:09:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1;
+randomNumber1 = Math.floor(Math.random() * 6)+ 1;
+var randomDiceImage = "dice" + randomNumber1+".png";
+var randomImageSource = "images/"+ randomDiceImage;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2;
+randomNumber2 = Math.floor(Math.random() * 6)+ 1;
+var randomDiceImage = "dice" + randomNumber2+".png";
+var randomImageSource = "images/"+ randomDiceImage;
+var image1= document.querySelectorAll("img")[1];
+image1.setAttribute("src",randomImageSource);
+
+
+if (randomNumber1> randomNumber2){
+  document.querySelector("h1").innerHTML = "Congrats! Player 1 wins.";
+} else if (randomNumber2> randomNumber1){
+    document.querySelector("h1").innerHTML = "Congrats! Player 2 wins.";
+}
+else{
+  document.querySelector("h1").innerHTML = "Oops! It's a draw";
+}
